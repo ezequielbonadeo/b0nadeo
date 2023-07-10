4d91ec6f1de3e5b9369ddc549e89dcd4a1c3ccc6
ezequielbonadeo <129343034+ezequielbonadeo@users.noreply.github.com>
2023-07-10 20:33:19 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
+navbar-light bg-light">
+<div class="container"> 
+<a class="navbar-brand" href="../templates/productos.html">Mishi Petshop</a>
+<img src="../images/21645.png">
+
+</div>
+</nav>`
